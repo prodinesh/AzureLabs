e8edeae47a1cafc7f3caa5cceff66a5fba039204
prodinesh <61177172+prodinesh@users.noreply.github.com>
2023-12-16 16:54:06 +0530

    Cleaned up obsolete files

--- AzureSQL_ServiceApp/Model/Courses.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AzureSQL_ServiceApp.Model
-{
-    public class Courses
-    {
-        public int CourseId { get; set; }
-        public string? CourseName { get; set; }
-        public int CourseFees { get; set; }
-    }
-}
